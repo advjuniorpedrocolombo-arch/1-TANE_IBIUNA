f1e37b7adf0ad4900e3bae486673b0aaa08c38cd
advjuniorpedrocolombo-arch <advjuniorpedrocolombo@gmail.com>
2026-09-20 16:54:19 -0300

    Padroniza CFE e PORA no modelo dinamico do 1 TANE

---
+++ pora/config.js
@@ -0,0 +1,5 @@
+window.PORTAL_CONFIG={
+  turma:'1º TANE - Extensão Ibiúna',
+  componente:'PORA',
+  apiUrl:'https://script.google.com/macros/s/AKfycbya3i18vWjiUcrfw7jIFzkwTDGPFEqCzMRWW2mbmQgipUoe06x-4-6IaGwCvRwU3ammKA/exec'
+};
